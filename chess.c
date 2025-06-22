ead58dded32e1f13e1937a1646aa6745c5da0831
Paloma Cristina <cristins.paloma@gmail.com>
2025-06-22 19:08:34 -0300

    refactor: rename variables for consistency and correctness

--- chess.c
+++ chess.c
@@ -1,43 +1,49 @@
 #include <stdio.h>
 
-int main() {
-    int towerSteps = 5;
-    int bishopSteps = 5;
-    int queenSteps = 8;
+int main()
+{
+    int towerMoves = 5;
+    int bishopMoves = 5;
+    int queenMoves = 8;
 
     int towerCounter;
     int bishopCounter = 1;
     int queenCounter = 1;
 
     printf("\nMovimento da Torre:\n");
-    for (towerCounter = 1; towerCounter <= towerSteps; towerCounter++) {
+    for (towerCounter = 1; towerCounter <= towerMoves; towerCounter++)
+    {
         printf("Direita\n");
     }
 
     printf("\nMovimento do Bispo:\n");
-    while (bishopCounter <= bishopSteps) {
+    while (bishopCounter <= bishopMoves)
+    {
         printf("Cima Direita\n");
         bishopCounter++;
     }
 
     printf("\nMovimento da Rainha:\n");
-    do {
+    do
+    {
         printf("Esquerda\n");
         queenCounter++;
-    } while (queenCounter <= queenSteps);
+    } while (queenCounter <= queenMoves);
 
-    
     printf("\nMovimento do Cavalo:\n");
-    int horseDownMoves = 2;
-    int horseLeftMoves = 1;
-    int horseMoveComplete = 1;
-    int horseCounter; 
+    int knightDownMoves = 2;
+    int knightLeftMoves = 1;
+    int knightMoveComplete = 1;
+    int knightCounter;
 
-    while (horseMoveComplete--) {
-        for (horseCounter = 0; horseCounter < horseDownMoves; horseCounter++) {
+        while (knightMoveComplete--)
+    {
+        for (knightCounter = 0; knightCounter < knightDownMoves; knightCounter++)
+        {
             printf("Baixo\n");
         }
-        for (horseCounter = 0; horseCounter < horseLeftMoves; horseCounter++) {
+        for (knightCounter = 0; knightCounter < knightLeftMoves; knightCounter++)
+        {
             printf("Esquerda\n");
         }
     }
