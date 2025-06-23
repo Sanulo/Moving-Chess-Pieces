f6975e1327b176990b55cfde9b223506bb2175e8
Paloma Cristina <cristins.paloma@gmail.com>
2025-06-22 23:49:14 -0300

    refactor: restructured piece movements using recursion and nested loops

--- chess.c
+++ chess.c
@@ -1,52 +1,66 @@
 #include <stdio.h>
 
-int main()
-{
-    int towerMoves = 5;
-    int bishopMoves = 5;
-    int queenMoves = 8;
+void towerMovement(int remainingMoves) {
+    if (remainingMoves == 0) {
+        return;
+    }
+    printf("Direita\n");
+    towerMovement(remainingMoves - 1);
+}
 
-    int towerCounter;
-    int bishopCounter = 1;
-    int queenCounter = 1;
+void queenMovement(int remainingMoves) {
+    if (remainingMoves == 0) {
+        return;
+    }
+    printf("Esquerda\n");
+    queenMovement(remainingMoves - 1);
+}
 
-    printf("\nMovimento da Torre:\n");
-    for (towerCounter = 1; towerCounter <= towerMoves; towerCounter++)
-    {
+void bishopMovement(int verticalMoves, int horizontalMoves) {
+    if (verticalMoves == 0) {
+        return;
+    }
+
+    printf("Cima\n");
+
+    for (int i = 0; i < horizontalMoves; i++) {
         printf("Direita\n");
     }
 
-    printf("\nMovimento do Bispo:\n");
-    while (bishopCounter <= bishopMoves)
-    {
-        printf("Cima Direita\n");
-        bishopCounter++;
-    }
+    bishopMovement(verticalMoves - 1, horizontalMoves);
+}
 
-    printf("\nMovimento da Rainha:\n");
-    do
-    {
-        printf("Esquerda\n");
-        queenCounter++;
-    } while (queenCounter <= queenMoves);
+void knightMovement() {
+    int upMoves = 2;
+    int rightMoves = 1;
 
     printf("\nMovimento do Cavalo:\n");
-    int knightDownMoves = 2;
-    int knightLeftMoves = 1;
-    int knightMoveComplete = 1;
-    int knightCounter;
 
-        while (knightMoveComplete--)
-    {
-        for (knightCounter = 0; knightCounter < knightDownMoves; knightCounter++)
-        {
-            printf("Baixo\n");
-        }
-        for (knightCounter = 0; knightCounter < knightLeftMoves; knightCounter++)
-        {
-            printf("Esquerda\n");
-        }
+    for (int i = 0; i < upMoves; i++) {
+        printf("Cima\n");
     }
+
+    for (int counter = 0; counter < rightMoves; counter++) {
+        printf("Direita\n");
+    }
+}
+
+int main() {
+    int towerMoves = 5;
+    int bishopVerticalMoves = 5;
+    int bishopHorizontalMoves = 1;
+    int queenMoves = 8;
+
+    printf("\nMovimento da Torre:\n");
+    towerMovement(towerMoves);
+
+    printf("\nMovimento do Bispo:\n");
+    bishopMovement(bishopVerticalMoves, bishopHorizontalMoves);
+
+    printf("\nMovimento da Rainha:\n");
+    queenMovement(queenMoves);
+
+    knightMovement();
 
     return 0;
 }
